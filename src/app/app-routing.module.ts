be564ce90f55eb22dbf8b003191e049710fbc5c4
PeeraponGongthong <152461229+PeeraponGongthong@users.noreply.github.com>
2023-12-04 21:03:40 +0700

    test 04/12/23

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+//   {
+//   path:'',
+//   component:LoginComponent,
+// },{
+//   path:'A',
+//   component:ResgistComponent
+// },
+// {
+//   path:'C',
+//   component:ResgistComponent,
+//   children:[
+//     {
+//       path:'A',
+//       component:LoginComponent
+//     }
+//   ]
+// }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
